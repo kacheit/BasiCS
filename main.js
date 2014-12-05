6902134d3acbeb12330d655521f7daa72c54cfd7
Kalon <kalon.cheong@gmail.com>
2014-12-04 20:08:58 -0500

    skeleton HTML
    -added bootstrap
    -added some CSS
    -left navbar somewhat complete

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$('#data-structures').click(function() {
+		$('#data-structures-list').toggle();
+	});
+
+	$('#algorithms').click(function() {
+		$('#algorithms-list').toggle();
+	});
+
+});
